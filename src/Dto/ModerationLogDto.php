b1e0bc88550d013c08cf862309fbf8a6698c2533
Guillaume Gomez <guillaume.gomez846@gmail.com>
2025-12-02 22:35:45 +0100

    create a new DTO to store moderatioLog + remove unused Json class as import

--- src/Dto/ModerationLogDto.php
+++ src/Dto/ModerationLogDto.php
@@ -3,34 +3,24 @@ namespace App\Dto;
 
 use Symfony\Component\Validator\Constraints as Assert;
 
-class VideoDto
+class ModerationLogDto
 {
-    public const STATUS_PENDING = 'pending';
-    public const STATUS_SPAM = 'spam';
-    public const STATUS_NOT_SPAM = 'not_spam';
-
-
     public function __construct(
         public readonly ?int $id = null,
 
         #[Assert\NotBlank(message: "Video ID is required")]
-        public readonly string $dailymotionVideoId,
+        public readonly int $videoId,
 
         #[Assert\Choice(
             choices: ['pending', 'spam', 'not_spam'],
             message: "Invalid video status"
         )]
-        public string $status = self::STATUS_PENDING,
+        public string $status = VideoDto::STATUS_PENDING,
 
         #[Assert\NotNull(message: "Created timestamp is required")]
         //private ?\DateTimeImmutable $createdAt = null,
         public $createdAt = null,
-
-        #[Assert\NotNull(message: "Updated timestamp is required")]
-        //private ?\DateTimeImmutable $updatedAt = null,
-        public $updatedAt = null,
     ) {
         $this->createdAt = new \DateTimeImmutable($createdAt ? $createdAt : "");
-        $this->updatedAt = new \DateTimeImmutable($updatedAt ? $updatedAt : "");
     }
 }
